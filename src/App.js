788dbf61ab22816a2b6fa295764c7058d877245c
Vilmar Cabanero <vilmarcabanero@gmail.com>
2021-06-12 20:27:54 +0800

    243. Working with Multiple Slices

--- src/App.js
+++ src/App.js
@@ -1,10 +1,15 @@
 import Counter from './components/Counter';
-// import Auth from './components/Auth';
+import Auth from './components/Auth';
+import { Fragment } from 'react';
+import Header from './components/Header';
 
 function App() {
 	return (
-		<Counter />
-		// <Auth />
+		<Fragment>
+			<Header />
+			<Auth />
+			<Counter />
+		</Fragment>
 	);
 }
 
--- src/store/index.js
+++ src/store/index.js
@@ -1,9 +1,9 @@
 import { createSlice, configureStore } from '@reduxjs/toolkit';
 
-const initialState = { counter: 0, showCounter: true };
+const initialCounterState = { counter: 0, showCounter: true };
 const counterSlice = createSlice({
 	name: 'counter',
-	initialState,
+	initialCounterState,
 	reducers: {
 		increment(state) {
 			state.counter++;
@@ -20,10 +20,30 @@ const counterSlice = createSlice({
 	},
 });
 
+const initialAuthState = {
+	isAuthenticated: false,
+};
+const authSlice = createSlice({
+	name: 'authentication',
+	initialAuthState,
+	reducers: {
+		login(state) {
+			state.isAuthenticated = true;
+		},
+		logout(state) {
+			state.isAuthenticated = false;
+		},
+	},
+});
+
 export const counterActions = counterSlice.actions;
+export const authActions = authSlice.actions;
 
 const store = configureStore({
-	reducer: counterSlice.reducer,
+	reducer: {
+		counter: counterSlice.reducer,
+		auth: authSlice.reducer,
+	},
 });
 
 export default store;
