6cb404285995c65b66b68db96ea362ea1761e668
Vilmar Cabanero <vilmarcabanero@gmail.com>
2021-06-12 21:10:58 +0800

    244. Reading & Dispatching From A New Slice

--- src/components/Auth.js
+++ src/components/Auth.js
@@ -1,18 +1,19 @@
 import classes from './Auth.module.css';
-import { useSelector, useDispatch } from 'react-redux';
+import { useDispatch } from 'react-redux';
 import { authActions } from '../store/index';
 
 const Auth = () => {
 	const dispatch = useDispatch();
 
-	const loginHandler = () => {
+	const loginHandler = (e) => {
+    e.preventDefault()
 		dispatch(authActions.login());
 	};
 
 	return (
 		<main className={classes.auth}>
 			<section>
-				<form>
+				<form onSubmit={loginHandler}>
 					<div className={classes.control}>
 						<label htmlFor='email'>Email</label>
 						<input type='email' id='email' />
@@ -21,7 +22,7 @@ const Auth = () => {
 						<label htmlFor='password'>Password</label>
 						<input type='password' id='password' />
 					</div>
-					<button onClick={loginHandler}>Login</button>
+					<button type='submit'>Login</button>
 				</form>
 			</section>
 		</main>
