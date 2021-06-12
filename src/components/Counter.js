2f36646d1fa2e3f20a440db6b0746606312083ae
Vilmar Cabanero <vilmarcabanero@gmail.com>
2021-06-12 18:08:34 +0800

    233. Using Redux Data in React Components

--- src/components/Counter.js
+++ src/components/Counter.js
@@ -1,15 +1,18 @@
 import classes from './Counter.module.css';
+import { useSelector, connect } from 'react-redux';
 
 const Counter = () => {
-  const toggleCounterHandler = () => {};
+  const counter = useSelector(state => state.counter)
 
-  return (
-    <main className={classes.counter}>
-      <h1>Redux Counter</h1>
-      <div className={classes.value}>-- COUNTER VALUE --</div>
-      <button onClick={toggleCounterHandler}>Toggle Counter</button>
-    </main>
-  );
+	const toggleCounterHandler = () => {};
+
+	return (
+		<main className={classes.counter}>
+			<h1>Redux Counter</h1>
+			<div className={classes.value}>{counter}</div>
+			<button onClick={toggleCounterHandler}>Toggle Counter</button>
+		</main>
+	);
 };
 
 export default Counter;
